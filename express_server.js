b39651b31e6f96ed4cba566e1a488366d30c26e8
John O'Halloran <john.ohalloran@telus.com>
2022-11-29 10:01:50 -0500

    eslinted express_server.js

--- express_server.js
+++ express_server.js
@@ -21,9 +21,9 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-const generateRandomString = () => {
-  return ((Math.random() + 1)* 0x10000).toString(36).substring(6);
-}
+const generateRandomString = () => { // Generates random 6 character string for new URL
+  return ((Math.random() + 1) * 0x10000).toString(36).substring(6);
+};
 
 
 // ------------------------------------ DATABASE -------------------------------------//
@@ -37,7 +37,7 @@ const urlDatabase = {
 // ------------------------------------- ROUTES -------------------------------------//
 app.get("/", (req, res) => {
   res.send("Hello!");
-  console.log(`client request to view homepage.`)
+  console.log(`client request to view homepage.`);
 });
 
 app.listen(PORT, () => {
@@ -46,7 +46,7 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-  console.log(`client request for urlDatabase.json file`)
+  console.log(`client request for urlDatabase.json file`);
 });
 
 // Example to use HTML Tags inside of VS (Not Required...)
@@ -56,50 +56,50 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-  console.log(`Client request to view URLs index`)
+  console.log(`Client request to view URLs index`);
 });
 
 app.get("/urls/new", (req, res) => { // page to create new URL if not in database
   res.render("urls_new");
-  console.log(`Client request to view URL creation page`)
+  console.log(`Client request to view URL creation page`);
 });
 
 // TO DO [ ] : Check for duplicate key values as edge case
 // TO DO [ ] : Check for missing www. and reject as invalid (maybe)
 
 app.post("/urls", (req, res) => {
-  console.log(`Client request to add short url: ${req.body}`); // Log the POST request body to the console
-  const randomName = generateRandomString()
-  const newLongUrl = req.body.longURL
+  console.log(`Client request to add short url: ${req.body}`);
+  const randomName = generateRandomString();
+  const newLongUrl = req.body.longURL;
   if (newLongUrl.slice(0,8) === 'https://' || newLongUrl.slice(0,7) === 'http://') {
-    urlDatabase[randomName] = newLongUrl  // check if contains http: already
+    urlDatabase[randomName] = newLongUrl;  // check if contains http: already
   } else {
-    urlDatabase[randomName] = `https://${newLongUrl}`  // check if contains http: already
+    urlDatabase[randomName] = `https://${newLongUrl}`;  // check if contains http: already
   }
-  res.redirect(`/urls/${randomName}`)
+  res.redirect(`/urls/${randomName}`);
 });
 
 app.get("/urls/:id", (req, res) => { // Redirect to summary ID page
-  const id = req.params.id
-  const longURL = urlDatabase[id]
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
   const templateVars = { id, longURL};
   res.render("urls_show", templateVars);
-  console.log(`Client request to view ${id}(${longURL}) summary page`)
+  console.log(`Client request to view ${id}(${longURL}) summary page`);
 });
 
 app.get("/u/:id", (req, res) => {  // Redirect to actual website
-  const id = req.params.id
-  const longURL = urlDatabase[id]
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
   //console.log(urlDatabase[id])
-  console.log(`Client redirect request for: ${id}(${longURL})`)
+  console.log(`Client redirect request for: ${id}(${longURL})`);
   res.redirect(longURL);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id
-  const longURL = urlDatabase[id]
-  console.log(`Client delete request for: ${id}(${longURL})`)
-  delete urlDatabase[id] // Delete requested item set by delete button
-  res.redirect(`/urls`)
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  console.log(`Client delete request for: ${id}(${longURL})`);
+  delete urlDatabase[id]; // Delete requested item set by delete button
+  res.redirect(`/urls`);
 });
 
