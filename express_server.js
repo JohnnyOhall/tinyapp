b5fb62c51b8b3ac52975809bd23f7ff95705190f
John O'Halloran <john.ohalloran@telus.com>
2022-11-29 11:25:23 -0500

    added url update POST route, added form submission in urls_show and new edit button in urls_index. Eslint express_server.js

--- express_server.js
+++ express_server.js
@@ -72,7 +72,7 @@ app.post("/urls", (req, res) => {
   const randomName = generateRandomString();
   const newLongUrl = req.body.longURL;
   if (newLongUrl.slice(0,8) === 'https://' || newLongUrl.slice(0,7) === 'http://') {
-    urlDatabase[randomName] = newLongUrl;  // check if contains http: already
+    urlDatabase[randomName] = newLongUrl;  // do not add https:// if already included in address
   } else {
     urlDatabase[randomName] = `https://${newLongUrl}`;  // check if contains http: already
   }
@@ -84,22 +84,45 @@ app.get("/urls/:id", (req, res) => { // Redirect to summary ID page
   const longURL = urlDatabase[id];
   const templateVars = { id, longURL};
   res.render("urls_show", templateVars);
-  console.log(`Client request to view ${id}(${longURL}) summary page`);
+  console.log(`Client request to view ${id} (${longURL}) summary page`);
 });
 
 app.get("/u/:id", (req, res) => {  // Redirect to actual website
   const id = req.params.id;
   const longURL = urlDatabase[id];
   //console.log(urlDatabase[id])
-  console.log(`Client redirect request for: ${id}(${longURL})`);
+  console.log(`Client redirect request for: ${id} (${longURL})`);
   res.redirect(longURL);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  console.log(`Client delete request for: ${id}(${longURL})`);
+  console.log(`Client delete request for: ${id} (${longURL})`);
   delete urlDatabase[id]; // Delete requested item set by delete button
   res.redirect(`/urls`);
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  const id = req.params.id;
+  let longURL = urlDatabase[id];
+  const editedUrl = req.body.editUrl;
+
+  console.log(`Client update request for: ${id} (${longURL})`);
+
+  if (editedUrl.slice(0,8) === 'https://' || editedUrl.slice(0,7) === 'http://') {
+    urlDatabase[id] = editedUrl; // do not add https:// if already included in address
+  } else {
+    urlDatabase[id] = `https://${editedUrl}`;  // check if contains http: already
+  }
+
+  res.redirect(`/urls`); // redirect to URLs index
+  longURL = urlDatabase[id]; // update longURL variable after edit
+  console.log(`New URL: ${id}(${longURL})`);
+});
+
+app.post("/urls/:id/", (req, res) => {
+  const id = req.params.id;
+  res.redirect(`/urls/${id}`);
 });
 
